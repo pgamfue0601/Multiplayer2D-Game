4b260c5f29a722651ddbd75fcaeaf8b48c9ed7b9
pgamfue0601 <pablo.gamiz.fuentes@hotmail.com>
2024-02-07 12:25:52 +0100

    Subida proyecto

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI puntuacion;
+    private AudioSource coinPicked;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        coinPicked = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            coinPicked.Play();
+            puntuacion.text = (Int32.Parse(puntuacion.text) + 10).ToString();
+            Destroy(gameObject, 0.8f);
+        }  
+    }
+}
