8117682a9dd8e54a8d7a7ceb9ee287d049136214
pgamfue0601 <pablo.gamiz.fuentes@hotmail.com>
2024-02-22 10:14:14 +0100

    Ajustes meta juego

--- Assets/Scripts/FinishGoal.cs
+++ Assets/Scripts/FinishGoal.cs
@@ -8,6 +8,7 @@ public class FinishGoal : MonoBehaviour
 {
     [SerializeField] private AudioSource m_AudioSource;
     private AudioSource win;
+    private bool finished = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,22 +28,23 @@ public class FinishGoal : MonoBehaviour
 
     private IEnumerator EndGoal(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !finished)
         {
+            finished = true;
             m_AudioSource.Stop();
             if (collision.GetComponent<SinglePlayerController>())
             {
                 collision.GetComponent<SinglePlayerController>().rb.velocity = Vector3.zero;
-                collision.GetComponent<SinglePlayerController>().rb.AddForce(collision.GetComponent<SinglePlayerController>().gameObject.transform.up * 325, ForceMode2D.Impulse);
+                collision.GetComponent<SinglePlayerController>().rb.AddForce(collision.GetComponent<SinglePlayerController>().gameObject.transform.up * 5, ForceMode2D.Impulse);
             }
 
             if (collision.GetComponent<PlayerController>())
             {
                 collision.GetComponent<SinglePlayerController>().rb.velocity = Vector3.zero;
-                collision.GetComponent<PlayerController>().rb.AddForce(collision.GetComponent<PlayerController>().gameObject.transform.up * 325, ForceMode2D.Impulse);
+                collision.GetComponent<PlayerController>().rb.AddForce(collision.GetComponent<PlayerController>().gameObject.transform.up * 5, ForceMode2D.Impulse);
             }
             win.Play();
-            yield return new WaitForSeconds(9f);
+            yield return new WaitForSeconds(8f);
             Debug.Log("Juego finalizado");
             SceneManager.LoadScene("MenuScene");
         }
